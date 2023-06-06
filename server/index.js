eb36ffab6fcf5cfaed8bef8531102e2ceb480052
zacharyfarber <zachfarber001@icloud.com>
2023-06-06 15:14:32 -0400

    established connection to mongodb database using mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('../config/devKeys');
 
 // CREATE EXPRESS INSTANCE
 const app = express();
+
+// CONNECT TO DATABASE
+mongoose.connect(keys.mongoURI);
 
 // TELL EXPRESS INSTANCE TO USE MIDDLEWARES
 app.use(bodyParser.urlencoded({ extended: false }));
