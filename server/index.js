d09cffb9b3a2da0a908b36fe79938d6977ccb7cb
zacharyfarber <zachfarber001@icloud.com>
2023-06-06 21:33:05 -0400

    created middleware to set headers for printful routes

--- server/index.js
+++ server/index.js
@@ -10,9 +10,21 @@ const app = express();
 // CONNECT TO DATABASE
 mongoose.connect(keys.mongoURI);
 
-// TELL EXPRESS INSTANCE TO USE MIDDLEWARES
+// TELL EXPRESS INSTANCE TO USE BODYPARSER MIDDLEWARES
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// TELL EXPRESS INSTANCE TO SET THE HEADERS ATTRIBUTE FOR PRINTFUL ROUTES
+app.use(function (req, res, next) {
+    const matchUrl = 'https://api.printful.com';
+
+    if (req.url.substring(0, matchUrl.length) === matchUrl) {
+        res.setHeader('Authorization', `Bearer ${keys.printfulToken}`);
+        res.setHeader('X-PF-Store-Id', keys.printfulStoreId);
+    }
+
+    return next();
+});
 
 // RUN PRINTFUL ROUTES
 printfulRouter(app);
