840910d71f1e033ed7ff58137717b361cad5d3a7
zacharyfarber <zachfarber001@icloud.com>
2023-06-08 03:08:54 -0400

    building the header and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import Header from './components/Header';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <div>
+            <Header />
             <Home />
+            <Footer />
         </div>
     );
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,19 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the home page', () => {
+test('renders the header component, the home page and the footer component', () => {
     // RENDER COMPONENT
     render(<App />);
 
     // FIND TEXT ON THE SCREEN
+    const header = screen.getByText(/header/i);
     const motto = screen.getByText(/motto/i);
     const music = screen.getByText(/music/i);
     const featured = screen.getByText(/featured/i);
     const mailing = screen.getByText(/mailing/i);
+    const footer = screen.getByText(/footer/i);
 
     // ASSERT ALL TEXT IS IN THE DOCUMENT
+    expect(header).toBeInTheDocument();
     expect(motto).toBeInTheDocument();
     expect(music).toBeInTheDocument();
     expect(featured).toBeInTheDocument();
     expect(mailing).toBeInTheDocument();
+    expect(footer).toBeInTheDocument();
 });
